d43c2f47777e3638bb9416e965fe14ac90b38ace
ShaheenSolwa <56385860+ShaheenSolwa@users.noreply.github.com>
2020-07-07 20:54:54 +0200

    Added Java Code with Forms

---
+++ OtherMathops.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TransmissionLines;
+
+/**
+ *
+ * @author shahe
+ */
+public class OtherMathops {
+    public double arctanh(double a){
+        return 0.5*Math.log(a);
+    }
+}
